618cf8552e5cf26371c9990fc9b22245fce599dc
kcjob <30427093+kcjob@users.noreply.github.com>
2017-12-11 10:25:30 -0500

    Update AccessCSVData.php

--- Apps/AccessCSVData.php
+++ Apps/AccessCSVData.php
@@ -17,9 +17,7 @@ class AccessCSVData {
     while($line = fgetcsv($fileHandle))
     {
       if (is_numeric($line[1]) and !$billObject->userName){
-        // echo "inside if \r\n";
-        // echo 'name: ' . $line[2] . "\r\n";
-        // echo 'PI Number: ' . $line[1] . "\r\n";
+
         $billObject -> userName = $line[2];
         $billObject -> userEmail = $line[3];
 
@@ -28,16 +26,8 @@ class AccessCSVData {
         if ($line[0]){
           array_push($billObject -> attachmentArray, $line[0]);
         }
-        // $count = 1;
-        // echo "pass: " . $count . "\r\n";
-        // $count++;
 
       } elseif (($line[2] == $billObject->userName) and is_numeric($line[1])){
-        // echo "inside 1st elseif \r\n";
-        // echo 'name: ' . $line[2] . "\r\n";
-        // echo 'PI Number: ' . $line[1] . "\r\n";
-        // echo "pass " . $count . "\r\n";
-        // $count++;
 
         array_push($billObject -> serviceInfoArray, ['service' => $billObject -> service = $line[5], 'startTime' => $billObject -> startTime = $line[6], 'endTime' => $billObject -> endTime = $line[7], 'serviceHours' => $billObject -> serviceHours = $line[8], 'notes' => $billObject -> notes = $line[9]]);
 
@@ -53,13 +43,6 @@ class AccessCSVData {
         echo '*CONFIGURE AND SEND EMAIL *' . "\r\n";
         echo 'ß**************************' . "\r\n";
 
-        // $count = 1;
-        // echo "inside 2nd elseif \r\n";
-        // echo "pass " . $count . "\r\n";
-        // $count++;
-        // echo 'name: ' . $line[2] . "\r\n";
-        // echo 'PI Number: ' . $line[1] . "\r\n";
-
         $billObject = new billingDetails();
         $billObject -> userName = $line[2];
         $billObject -> userEmail = $line[3];
@@ -71,8 +54,5 @@ class AccessCSVData {
         }
       }
     } //while
-    var_dump($billObject);
-    //echo 'name: ' . $line[2] . "\r\n";
-    //echo 'PI Number: ' . $line[1] . "\r\n";
   }
 }
