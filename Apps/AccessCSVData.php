d987616c8b4502f732db92e7056976f7fc75d270
kcjob <30427093+kcjob@users.noreply.github.com>
2017-12-11 12:25:20 -0500

    Updated files

--- Apps/AccessCSVData.php
+++ Apps/AccessCSVData.php
@@ -18,9 +18,6 @@ class AccessCSVData {
     while($line = fgetcsv($fileHandle))
     {
       if (is_numeric($line[1]) and !$billObject->userName){
-        // echo "inside if \r\n";
-        // echo 'name: ' . $line[2] . "\r\n";
-        // echo 'PI Number: ' . $line[1] . "\r\n";
         $billObject -> userName = $line[2];
         $billObject -> userEmail = $line[3];
 
@@ -29,16 +26,8 @@ class AccessCSVData {
         if ($line[0]){
           array_push($billObject -> attachmentArray, $line[0]);
         }
-        // $count = 1;
-        // echo "pass: " . $count . "\r\n";
-        // $count++;
 
       } elseif (($line[2] == $billObject->userName) and is_numeric($line[1])){
-        // echo "inside 1st elseif \r\n";
-        // echo 'name: ' . $line[2] . "\r\n";
-        // echo 'PI Number: ' . $line[1] . "\r\n";
-        // echo "pass " . $count . "\r\n";
-        // $count++;
 
         array_push($billObject -> serviceInfoArray, ['service' => $billObject -> service = $line[5], 'startTime' => $billObject -> startTime = $line[6], 'endTime' => $billObject -> endTime = $line[7], 'serviceHours' => $billObject -> serviceHours = $line[8], 'notes' => $billObject -> notes = $line[9]]);
 
@@ -48,20 +37,11 @@ class AccessCSVData {
 
       } elseif (($line[2] != $billObject->userName) and is_numeric($line[1])){
 
-        //var_dump($billObject);
-
         echo '***************************' . "\r\n";
         echo '*CONFIGURE AND SEND EMAIL *' . "\r\n";
         echo 'ß**************************' . "\r\n";
 
         EmailMessageGenerator::createEmail($billObject);
-
-        // $count = 1;
-        // echo "inside 2nd elseif \r\n";
-        // echo "pass " . $count . "\r\n";
-        // $count++;
-        // echo 'name: ' . $line[2] . "\r\n";
-        // echo 'PI Number: ' . $line[1] . "\r\n";
 
         $billObject = new billingDetails();
         $billObject -> userName = $line[2];
@@ -69,7 +49,7 @@ class AccessCSVData {
 
         array_push($billObject -> serviceInfoArray, ['service' => $billObject -> service = $line[5], 'startTime' => $billObject -> startTime = $line[6], 'endTime' => $billObject -> endTime = $line[7], 'serviceHours' => $billObject -> serviceHours = $line[8], 'notes' => $billObject -> notes = $line[9]]);
 
-        if ($line[0]){
+        if($line[0]){
           array_push($billObject -> attachmentArray, $line[0]);
         }
       }
@@ -79,8 +59,5 @@ class AccessCSVData {
       EmailMessageGenerator::createEmail($billObject);
       fclose($fileHandle);
     }
-    //var_dump($billObject);
-    //echo 'name: ' . $line[2] . "\r\n";
-    //echo 'PI Number: ' . $line[1] . "\r\n";
   }
 }
