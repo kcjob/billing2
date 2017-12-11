36f427d559b748abd39f66b731635766f9eb439b
kcjob <30427093+kcjob@users.noreply.github.com>
2017-12-11 12:22:16 -0500

    Update start.php

--- start.php
+++ start.php
@@ -13,28 +13,3 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 $billObject = AccessCSVData::getCsvDataAsArray();
-//var_dump($billObject);
-//$arrayofbillObjects = BillObject::createBillObject($csvDataArray);
-//$arrayOfMsgDataObjects = //MessageDataFromBillObject::getMessageData($arrayofbillObjects);
-//EmailMessageGenerator::createEmail($arrayOfMsgDataObjects);
-
-
-
-
-
-//echo $msg;
-/*
-$template = new TemplateView();
-$template -> generateView($arrayOfMsgDataObjects);
-
- $loader = new Twig_Loader_Filesystem('Templates');
-$twig = new Twig_Environment($loader, array('debug' => true));
-$twig->addExtension(new Twig_Extension_Debug());
-$template = $twig->load('messagebody.html.twig');
-
-//----- split out each msg object
-foreach($arrayOfMsgDataObjects as $msgDataObject){
-//var_dump($dataObject->userName);
-echo $template->render(array('dataObject'=>$msgDataObject));
-}
-*/
